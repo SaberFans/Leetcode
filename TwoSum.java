3f6f8b15c40da0775a3ed2a42e9d1e9133b6922e
twang123 <twang123@users.noreply.github.com>
2014-06-02 23:55:31 +0100

    two sum first attempt

---
+++ TwoSum.java
@@ -0,0 +1,15 @@
+package LinearList;
+
+import java.util.Map;
+
+public class TwoSum {
+	static public Map<Integer,Integer> twoSum(int[]num, int target){
+		Map<Integer,Integer> ans = new HashMap<>();
+		return ans;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
