cda5caa06ed1ea0fddef0d13cd2b37b42e5f0f60
twang123 <twang123@users.noreply.github.com>
2014-06-08 23:41:08 +0100

    four sum first created

---
+++ FourSum.java
@@ -0,0 +1,16 @@
+package LinearList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FourSum {
+	public List<List<Integer>> fourSum(int[] num, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        return res;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
