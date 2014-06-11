6c3b4ffbba206d9b3226691c07d038d0a5911e54
twang123 <twang123@users.noreply.github.com>
2014-06-11 16:22:00 +0100

    Update FourSum.java
    cubic time solution, first attempt

--- FourSum.java
+++ FourSum.java
@@ -4,13 +4,56 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FourSum {
-	public List<List<Integer>> fourSum(int[] num, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        return res;
-    }
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int partition(int[]num,int first,int end){
+		int pivot = first;
+		for(int i=first;i<end-1;i++){
+			if(num[i]<num[end-1]){
+				int tmp = num[i];
+				num[i] = num[pivot];
+				num[pivot] = tmp;
+				pivot++;
+			}
+		}
+		int tmp = num[end-1];
+		num[end-1] = num[pivot];
+		num[pivot] = tmp;
+		return pivot;
 	}
-
+	static void quicksort(int[]num,int first, int end){
+		if(first<end-1){
+			int pivot = partition(num,first,end);
+			quicksort(num,first,pivot);
+			quicksort(num,pivot+1,end);
+		}
+	}
+	static public List<List<Integer>> fourSum(int[] num, int target) {
+		quicksort(num,0,num.length);
+	 	List<List<Integer>> res = new ArrayList<>();
+        	for(int i=0;i<num.length-3;i++){
+        		for(int j=i+1;j<num.length-2;j++){
+	        		int first = j+1, end =num.length-1;
+	        		while(first<end){
+	        			int sum = num[first]+num[i]+num[j]+num[end];
+	        			if(sum>target){
+	        				end--;
+	        			}
+	        			else if(sum<target){
+	        				first++;
+	        			}
+	        			else{
+	        				res.add(Arrays.asList(num[i],num[j],num[first],num[end]));
+	        				first++;
+	        				end--;
+	        				//return res;
+	        			}
+	        		}
+        		}
+        	}
+        	return res;
+    	}
+	public static void main(String[] args) {
+		int[]num= {1, 0, -1, 0, -2, 2};
+		List a =fourSum(num,0);
+		System.out.println(a);
+	}
 }
