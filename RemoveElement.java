db568e51afa102d5f946798ce114af8fd5a46572
twang123 <twang123@users.noreply.github.com>
2014-06-13 14:04:29 +0100

    Update the linear time solution.

--- RemoveElement.java
+++ RemoveElement.java
@@ -5,22 +5,29 @@ import java.util.Arrays;
 public class RemoveElement {
 
 	public static int removeElement(int[] A, int elem) {
-        int length = A.length;
-        for(int i=0;i<length;i++){
-        	if(A[i]==elem){
-        		int l = length;
-        		int j=i;
-        		int t=i;
-        		while(j<l&&A[j]==elem){
-        			j++;
-        			length--;
-        		}
-        		for(;j<l;j++,t++){
-        			A[t]=A[j];
-        		}
-        	}
-        }
-        return length;
+	        int length = A.length;
+	        for(int i=0;i<length;i++){
+	        	if(A[i]==elem){
+	        		int l = length;
+	        		int j=i;
+	        		int t=i;
+	        		while(j<l&&A[j]==elem){
+	        			j++;
+	        			length--;
+	        		}
+	        		for(;j<l;j++,t++){
+	        			A[t]=A[j];
+	        		}
+	        	}
+	        }
+	        return length;
+    }
+    public static int removeElement2(int[]A,int elem){
+    	int index=0;
+    	for(int a:A){
+    		if(a!=elem) A[index++] = a;
+    	}
+    	return index;
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
