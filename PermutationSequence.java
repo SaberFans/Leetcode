4cf3f4a5c1fd5bd454bf95db03d03b616af8a30f
twang123 <twang123@users.noreply.github.com>
2014-06-14 16:54:48 +0100

    permutation seq

---
+++ PermutationSequence.java
@@ -0,0 +1,13 @@
+package LinearList;
+
+public class PermutationSequence {
+	public String getPermutation(int n, int k) {
+        String res=null;
+        return res;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
