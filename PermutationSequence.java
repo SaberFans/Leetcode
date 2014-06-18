942969bc960040bc08251f5275ba7c79a3c40bd3
twang123 <twang123@users.noreply.github.com>
2014-06-18 08:04:48 +0100

    coding scheme, k/(n-i)!

--- PermutationSequence.java
+++ PermutationSequence.java
@@ -1,13 +1,31 @@
 package LinearList;
 
 public class PermutationSequence {
-	public String getPermutation(int n, int k) {
+	// coding scheme k/(n-i)!
+	public static String getPermutation(int n, int k) {
+		String seq = "";
+		for(int i=1;i<=n;i++){
+			seq += i;
+		}
+		
         String res=null;
+        
         return res;
     }
+	public static int factorial(int n){
+		int res=1;
+		for(int i=n;i>1;i--){
+			res*=i;
+		}
+		return res;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String seq =null;
+		for(int i=1;i<=5;i++){
+			seq = getPermutation(3,i);
+			System.out.println(seq);
+		}
 	}
 
 }
