994e0bc1827ae94f6f6cb33ca242efa6a450ca20
twang123 <wtyang926@gmail.com>
2015-01-10 13:12:22 +0000

    commit to merge

--- src/Backtracking/Combinations.java
+++ src/Backtracking/Combinations.java
@@ -7,44 +7,38 @@ import org.junit.Test;
 
 public class Combinations {
 
-	/** recursive way to generate combinations
-	 * @param combinations
+	/** Backtracking way to generate combinations
 	 * @param index
 	 * @param n
 	 * @param k
 	 */
-	void generateCombination(List<List<Integer>> combinations, int index, int n, int k) {
-		if(k==0){
-			return;
-		}
-		List<Integer> set = new ArrayList<Integer>();
-		
-		for(int i=index; i<n-k; i++){
-			set.add(i);
-		}
-		combinations.add(set);
-		generateCombination(combinations, index+1, n, k);
+    void generateCombination(List<Integer> tuple, int index, int n, int k) {
+
+        for(int i=index;i<n;i++){
+
+        }
+
 		
 	}
 
-	@Test
-	public void test(){
-		List<List<Integer>> lofsets = new ArrayList<>();
-		
-		 
-		
-		System.out.println(lofsets);
-	}
-	
-	@Test
-	public void test2(){
-		
-	}
-	
-	
 	public List<List<Integer>> combine(int n, int k) {
 		List<List<Integer>> lofsets = new ArrayList<>();
 
 		return lofsets;
 	}
+
+
+    @Test
+    public void test(){
+        List<List<Integer>> lofsets = new ArrayList<>();
+
+
+
+        System.out.println(lofsets);
+    }
+
+    @Test
+    public void test2(){
+
+    }
 }
