137809c4d7b63745204da18a648dbe3b1b318b04
twang123 <wtyang926@gmail.com>
2015-01-13 21:20:20 +0000

    commit to merge

--- src/Backtracking/GrayCode.java
+++ src/Backtracking/GrayCode.java
@@ -3,9 +3,9 @@ package Backtracking;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Gray Code stands for a successive values which differ only in one bit
@@ -16,36 +16,46 @@ import java.util.Set;
  * 2 - 10
  */
 public class GrayCode {
-    void generateGrayCode(int grayvalue, HashSet<Integer> checker, List<Integer> graynums, int size, int n) {
-        if (graynums.size() == size)
+    void generateGrayCode(int grayvalue, boolean checker[], List<Integer> graynums, int n) {
+        if (graynums.size() == checker.length)
             return;
 
         for (int i = 0; i < n; i++) {
-            int next = grayvalue - (int) Math.pow(i, 2);
+            int diff = (int) Math.pow(i, 2);
+            int next = grayvalue - diff;
 
             if (next > 0) {
-                if (!checker.contains(next)) {
+                if (checker[next]==false) {
 
                     graynums.add(next);
-                    checker.add(next);
-                    generateGrayCode(next, checker, graynums, size, n);
-                }
-            }
-            next = grayvalue + (int) Math.pow(i, 2);
-            if (next < size) {
-                if (!checker.contains(next)) {
-                    graynums.add(next);
-                    checker.add(next);
-                    generateGrayCode(next, checker,graynums, size, n);
+                    checker[next] = true;
+                    generateGrayCode(next, checker, graynums, n);
                 }
             }
         }
+
+        for (int i = 0; i < n; i++) {
+            int diff = (int) Math.pow(i, 2);
+            int next = grayvalue + diff;
+            if (next < graynums.size()) {
+                if (checker[next]==false) {
+                    graynums.add(next);
+                    checker[next] = true;
+                    generateGrayCode(next, checker,graynums, n);
+                }
+            }
+
+        }
+
     }
 
     public List<Integer> grayCode(int n) {
         List<Integer> graynums = new ArrayList<>();
+        int count = (int)Math.pow(n, 2);
+        boolean checker[] = new boolean[count];
+        Arrays.fill(checker, false);
 
-        generateGrayCode(0, new HashSet<Integer>(), graynums, (int) Math.pow(n, 2), n);
+        generateGrayCode(0, checker, graynums, (int) Math.pow(n, 2));
 
 
         return graynums;
