ab840df198f65d663865a9303de672ce0c307b2f
epttwxz <tianyang.wang@ericsson.com>
2015-01-20 11:16:54 +0000

    Update Tree structure for next right pointer.

--- src/Tree/TreeNode.java
+++ src/Tree/TreeNode.java
@@ -1,15 +1,17 @@
 package Tree;
 
 /**
- * Definition for binary tree: 
+ * Definition for binary tree:
  */
 public class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
+    
+    // new att for populating right pointer.
+    TreeNode next;
 
     TreeNode(int x) {
-        val = x;
+	val = x;
     }
 }
-
