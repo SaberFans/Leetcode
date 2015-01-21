ffdc0d52f32690acf3c9eaf2a24460bc4886cc0f
epttwxz <tianyang.wang@ericsson.com>
2015-01-21 18:00:51 +0000

    marked to be solved.

---
+++ src/DP/UniqueBinarySearchTrees.java
@@ -0,0 +1,16 @@
+package DP;
+
+/**
+ * Structurally unique BST's that store values 1...n. Input value is n;
+ *
+ */
+
+// Marker to be solved today.
+public class UniqueBinarySearchTrees {
+
+    public int numTrees(int n) {
+	int num = 0;
+	
+	return num;
+    }
+}
