387cd12346eb20ffeb3c8401a675498fea1fbe17
epttwxz <tianyang.wang@ericsson.com>
2015-01-22 10:17:28 +0000

    Sum of trees of N nodes depends on the 1~ n-1 nodes sub-tree combination on left or right.

--- src/DP/UniqueBinarySearchTrees.java
+++ src/DP/UniqueBinarySearchTrees.java
@@ -10,7 +10,16 @@ public class UniqueBinarySearchTrees {
 
     public int numTrees(int n) {
 	int num = 0;
-	
-	return num;
+        int count[] = new int[n+1];
+        count[0] = count[1] =1;
+        for(int i=2;i<=n;i++){
+            int sum = 0;
+            for(int k=0;k<i;k++ ){
+                sum += count[k]*count[i-1-k];
+            }
+            count[i] = sum;
+        }
+
+        return count[n];
     }
 }
