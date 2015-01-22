6e7b18720f33fccb5326ddb34e7ff28dd1666f68
epttwxz <tianyang.wang@ericsson.com>
2015-01-22 17:20:43 +0000

    Bottom-up has redundant steps. Try implement top-down solution.

---
+++ src/Tree/BalancedBST.java
@@ -0,0 +1,51 @@
+package Tree;
+
+/**
+ * Balanced Binary Search Tree problem. Difference of left branch's depth and
+ * right branch's depth of every node isn't bigger than 1.
+ */
+public class BalancedBST {
+    /***********************************************************************/
+    /**********Slow Implementation, redundant calculation for depth*********/
+    int depth(TreeNode root) {
+	if (root == null)
+	    return 0;
+	int letfDepth = 1 + depth(root.left);
+	int rightDepth = 1 + depth(root.right);
+
+	return Math.max(letfDepth, rightDepth);
+    }
+
+    public boolean isBalanced(TreeNode root) {
+	boolean depthBalanced = false;
+	
+	if(root==null)
+	    return true;
+	
+	int left = depth(root.left);
+	int right = depth(root.right);
+	
+	depthBalanced = Math.abs(left-right)>1?false:true;
+	
+	    
+	return depthBalanced && isBalanced(root.left) && isBalanced(root.right);
+    }
+    /***********************************************************************/
+    
+    /**
+     * Top-Down Strategy as the quick implementation.
+     * @param root
+     * @return
+     */
+    public boolean isBalancedQuick(TreeNode root){
+	boolean balance = false;
+	if(root==null)
+	    return false;
+	
+	boolean left =  isBalanced(root.left);
+	boolean right = isBalanced(root.right); 
+	
+	return balance;
+    }
+    
+}
