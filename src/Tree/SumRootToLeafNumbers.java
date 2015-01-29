69666f1d48efc3e7b5ff1edfe5cb3f005ccd9e8e
twang123 <wtyang926@gmail.com>
2015-01-29 22:12:15 +0000

    Calculate the sum of path at leaf node, and take in the sum then.

--- src/Tree/SumRootToLeafNumbers.java
+++ src/Tree/SumRootToLeafNumbers.java
@@ -1,32 +1,57 @@
 package Tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Sum of Root to Leaf numbers.
  */
 public class SumRootToLeafNumbers {
 
-    public int calculateValue(TreeNode root, int multiplier, int val){
-        if(root==null)
-            return val;
+    void calculateValue(TreeNode root, List<Integer> path, List<Integer> values){
+        if(root.left==null && root.right==null){
+            int sum = 0;
+            for(int i=path.size()-1, j=1;i>=0;i--, j*=10){
+                sum += path.get(i)*j;
+            }
+            values.add(sum);
 
-        int curVal = val + root.val*multiplier;
+        }
+        else if(root.left==null){
+            path.add(root.right.val);
+            calculateValue(root.right, path, values);
+            path.remove(path.size()-1);
+        }
+        else if(root.right==null){
+            path.add(root.left.val);
+            calculateValue(root.left, path, values);
+            path.remove(path.size()-1);
+        }
+        else{
+            path.add(root.left.val);
+            calculateValue(root.left, path, values);
+            path.remove(path.size()-1);
 
-        multiplier = multiplier*10;
-        if(root.left==null && root.right==null)
-            return curVal;
-        if(root.left==null)
-            return calculateValue(root.right, multiplier, curVal);
-        if(root.right==null)
-            return calculateValue(root.left, multiplier, curVal);
+            path.add(root.right.val);
+            calculateValue(root.right, path, values);
+            path.remove(path.size()-1);
+        }
 
-        return calculateValue(root.left, multiplier, curVal)+calculateValue(root.right, multiplier, curVal);
+
     }
-
     public int sumNumbers(TreeNode root) {
-        int sum;
+        int sum=0;
+        List<Integer> link = new ArrayList<>();
+        List<Integer> values = new ArrayList<>();
+        if(root==null)
+            return sum;
 
-        sum = calculateValue(root, 1, 0);
+        link.add(root.val);
+        calculateValue(root, link, values);
 
+        for(Integer i: values){
+            sum+=i;
+        }
         return sum;
     }
 }
