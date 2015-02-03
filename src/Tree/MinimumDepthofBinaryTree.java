e0308ef5e968fc888d7454584420017394189fa5
twang123 <wtyang926@gmail.com>
2015-02-03 19:51:06 +0000

    MiniumDepthtoLeafNode passed test.

--- src/Tree/MinimumDepthofBinaryTree.java
+++ src/Tree/MinimumDepthofBinaryTree.java
@@ -4,27 +4,22 @@ package Tree;
  * Given a binary tree, find its minimum depth.
  */
 public class MinimumDepthofBinaryTree {
-    int getMinDepth(TreeNode root, int depth){
-        if(root==null)
+    int getMinDepth(TreeNode root, int depth) {
+        if (root.left == null && root.right == null)
             return depth;
-        return Math.min(getMinDepth(root.left, depth + 1), getMinDepth(root.right, depth + 1));
+        else if (root.left != null && root.right != null) {
+            return Math.min(getMinDepth(root.left, depth + 1), getMinDepth(root.right, depth + 1));
+        } else if (root.left == null) {
+            return getMinDepth(root.right, depth + 1);
+        } else {
+            return getMinDepth(root.left, depth + 1);
+        }
     }
-    public int minDepth(TreeNode root) {
-        int depth = 0;
-        if(root==null)
-            return depth;
-        if(root.left==null && root.right==null){
-            return 1;
-        }
-        else if(root.left!=null && root.right!=null){
-            return Math.min(getMinDepth(root.left, 1), getMinDepth(root.right, 1));
-        }
-        else if(root.left!=null)
-            return getMinDepth(root.left, 1);
-        else{
-            return getMinDepth(root.right, 1);
-        }
 
+    public int minDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        return getMinDepth(root, 1);
     }
 }
 
