0b5ea4bf62d90a839fd32c93faebc91200dabc9f
epttwxz <tianyang.wang@ericsson.com>
2015-09-25 16:46:46 +0100

    second attempt, bit clean iteration process

--- src/Array/SummaryRanges.java
+++ src/Array/SummaryRanges.java
@@ -41,13 +41,11 @@ public class SummaryRanges {
 	public void valid_test5(){
 		List<String> res = new SummaryRanges().summaryRanges(new int[]{1,2,4,6});
 		List<String> match = Arrays.asList("1->2","4", "6");
-		System.out.println(res);
 		assertTrue(match.equals(res));
 	}
 	@Test
 	public void valid_test7(){
 		List<String> res = new SummaryRanges().summaryRanges(new int[]{1,2,4,5,6,8,10});
-		System.out.println();
 		List<String> match = Arrays.asList("1->2","4->6","8", "10");
 		
 		assertTrue(match.equals(res));
@@ -56,7 +54,6 @@ public class SummaryRanges {
 	@Test
 	public void valid_test6(){
 		List<String> res = new SummaryRanges().summaryRanges(new int[]{1,2,4,6,7,8,10});
-		System.out.println();
 		List<String> match = Arrays.asList("1->2","4", "6->8", "10");
 		
 		assertTrue(match.equals(res));
@@ -64,11 +61,15 @@ public class SummaryRanges {
 	@Test
 	public void integer_overflow_test(){
 		List<String> res = new SummaryRanges().summaryRanges(new int[]{-2147483648,-2147483647,2147483647});
-		System.out.println();
 		List<String> match = Arrays.asList("-2147483648->-2147483647","2147483647");
 		
 		assertTrue(match.equals(res));
 	}
+	/**
+	 * Iteration over the array and use start_num, end_num to keep the range of the ascending sequence.
+	 * @param nums
+	 * @return list of the continuous ranges.
+	 */
 	public List<String> summaryRanges(int[] nums) {
 		List<String> ranges = new ArrayList<>();
 		if(nums==null || nums.length==0)
@@ -77,35 +78,38 @@ public class SummaryRanges {
 		int end_num = start_num;
 		
 		for(int i=0;i<nums.length;i++){
-			if(i==nums.length-1){
-				if(nums[i]==end_num+1){
-					ranges.add(start_num+"->"+nums[i]);
-				}
-				else{
-					if(end_num!=nums[i]){
-						if(start_num==end_num)
-							ranges.add(""+start_num);
-						else
-							ranges.add(start_num+"->"+end_num);
-					}
-					ranges.add(""+nums[i]);
-				}
-			}
-			else if(nums[i]>end_num+1){
-				if(end_num==start_num){
-					ranges.add(""+end_num);
+			if(nums[i]> (long)end_num+1){
+				if(start_num==end_num){
+					ranges.add(""+start_num);
 				}
 				else{
 					ranges.add(start_num+"->"+end_num);
 				}
+				start_num = nums[i];
 				end_num = nums[i];
-				start_num= nums[i];
+				if(i==nums.length-1)
+					ranges.add(""+nums[i]);
 			}
-			else
+			else if(end_num==(long)nums[i]+1){
+				if(i==nums.length-1)
+					ranges.add(start_num+"->"+nums[i]);
+				else
+					end_num = nums[i];
+			}
+			else{
 				end_num = nums[i];
-			
+				if(i==nums.length-1){
+					if(start_num==end_num){
+						ranges.add(""+start_num);
+					}
+					else{
+						ranges.add(start_num+"->"+end_num);
+					}
+				}
+			}
 		}
-		 
+//		if(ranges.size()==0)
+//			ranges.add(""+nums[0]);
 		return ranges;
 	}
 }
