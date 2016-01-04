662588c1ee5cb8976e3d5f10e81668e383d49925
epttwxz <tianyang.wang@ericsson.com>
2016-01-04 17:36:55 +0000

    wrong ans...

--- src/util/sorting/MergeSort.java
+++ src/util/sorting/MergeSort.java
@@ -11,7 +11,8 @@ public class MergeSort {
 		if(A==null) return;
 		int length = A.length;
 		auxilary = new int[length];
-		System.arraycopy(A, 0, auxilary, 0, length);
+		//System.arraycopy(A, 0, auxilary, 0, length);
+		auxilary = A.clone();
 
 		sort(A, 0, length);
 		
@@ -42,10 +43,11 @@ public class MergeSort {
 	 * @param high right boundary for the current merge
 	 */
 	void merge(int A[], int low, int mid, int high){
-		
+		// copy original array into the axuliary array
 		for(int i=low; i<high ;i++){
 			auxilary[i] = A[i];
 		}
+		
 		int r = low, t = mid;
 		
 		for(int i=low;i<high;){
@@ -67,10 +69,47 @@ public class MergeSort {
 			}
 		}
 	}
+	
+	/*******************************************************************
+     *  Version of solution that eliminates the copy into auxiliary 
+     *  array.
+     *******************************************************************/
+	void sortX_entry(int[]src){
+		int[] dis = src.clone();
+		sortX(dis, src, 0, src.length);
+	}
+	void sortX(int[] src, int[] dist, int start, int end){
+		if(start+1>=end) return;
+		
+		int mid = (start+end)/2;
+		
+		sortX(dist, src, start, mid);
+		sortX(dist, src, mid+1, end);
+		mergeX(src, dist, start, mid, end);
+		
+	}
+	void mergeX(int[]src, int[]dis, int start, int mid, int end){
+		int low1 = start, low2 = mid+1;
+		for(int i= start;i<end;i++){
+			if(low1>mid){
+				dis[i] = src[low2++];
+			}
+			else if(low2>=end){
+				dis[i] = src[low1++];
+			}
+			else if(src[low1]>src[low2]){
+				dis[i] = src[low2++];
+			}
+			else
+				dis[i] = src[low1++];
+		}
+	}
+	
+	
 	@Test
 	public void test(){
-		int[]A = {3,2,1};
-		new MergeSort().sort(A);
+		int[]A = {3,2,3,3,1};
+		new MergeSort().sortX_entry(A);
 		System.out.println(Arrays.toString(A));
 	}
 }
