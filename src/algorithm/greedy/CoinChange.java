0c2ddd26d720bb1be2d40825560fac48e040800b
epttwxz <tianyang.wang@ericsson.com>
2016-04-19 17:14:13 +0100

    commit dp solution for coin change algorithm.

--- src/algorithm/greedy/CoinChange.java
+++ src/algorithm/greedy/CoinChange.java
@@ -42,9 +42,39 @@ public class CoinChange {
 		
 		return ArrayUtil.toArray(changeSet.toArray(new Integer[1]));
 	}
+	
+	int[] coinSet2 = {1, 3, 5};
+	
+	/**
+	 * DP solution for coin change minimum count.
+	 * 
+	 * @param coinVal
+	 * @return
+	 */
+	int[] getChangeGrid_DP(int coinVal){
+		int arr[] = new int[coinVal+1];
+		int coinSet_size = 3;
+		
+		for(int curCoinVal=1;curCoinVal<=coinVal;curCoinVal++){
+ 			
+			for(int j=0;j<coinSet_size;j++){
+				if (coinSet2[j] <= curCoinVal) {
+					if (arr[curCoinVal] == 0 || arr[curCoinVal]>arr[curCoinVal-coinSet2[j]]+1 )
+						arr[curCoinVal] = arr[curCoinVal-coinSet2[j]] + 1;
+				}
+			}
+		}
+		
+		return arr;
+	}
+	
 	@Test
 	public void testCoinChange() throws Exception {
 		System.out.println(Arrays.toString(greedyCashier(50)));
 	}
-	
+
+	@Test
+	public void testCoinChangeDP() throws Exception {
+		System.out.println(Arrays.toString(getChangeGrid_DP(10)));
+	}
 }
