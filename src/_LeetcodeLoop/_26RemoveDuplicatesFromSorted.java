47f485c0afc30b62a2a0769e18f7d5773fcd9932
twang123 <wtyang926@gmail.com>
2017-02-22 23:24:27 +0000

    [5_Easy_Array] start prepare algorithm test.

---
+++ src/_LeetcodeLoop/_26RemoveDuplicatesFromSorted.java
@@ -0,0 +1,20 @@
+package leetcode.easy;
+
+/**
+ * Created by yang on 2017/2/22.
+ */
+public class _26RemoveDuplicatesFromSorted {
+    public int removeDuplicates(int[]nums){
+        if(nums==null)
+            return 0;
+        if(nums.length<2)
+            return nums.length;
+        int cursor = 1;
+
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>nums[i-1])
+                nums[cursor++] = nums[i];
+        }
+        return cursor;
+    }
+}
---
+++ src/_LeetcodeLoop/_35SearchInsertPos.java
@@ -0,0 +1,22 @@
+package leetcode.easy;
+
+/**
+ * Created by yang on 2017/2/22.
+ */
+public class _35SearchInsertPos {
+    public int searchInsert(int[] nums, int target) {
+        if(nums==null || nums.length==0)
+            return -1;
+        int low = 0, high = nums.length-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]>target)
+                high -=1;
+            else if(nums[mid]==target)
+                return mid;
+            else
+                low+=1;
+        }
+        return low;
+    }
+}
