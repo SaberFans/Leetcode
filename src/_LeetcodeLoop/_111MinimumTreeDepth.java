a7703f483874f8fd864529b4f9e7224e4c7963cd
twang123 <wtyang926@gmail.com>
2017-03-24 00:24:47 +0000

    [easy_tree] minimum leaf node depth.

---
+++ src/_LeetcodeLoop/_111MinimumTreeDepth.java
@@ -0,0 +1,28 @@
+package _LeetcodeLoop;
+
+import Tree.TreeNode;
+import Tree.TreeUtil;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Given a binary tree, find its minimum depth.
+ * The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+ */
+public class _111MinimumTreeDepth {
+    public int minDepth(TreeNode root) {
+        if(root==null)
+            return 0;
+        int leftMin = minDepth(root.left);
+        int rightMin = minDepth(root.right);
+        return (leftMin==0? rightMin: (rightMin==0?leftMin: Math.min(leftMin,rightMin))) +1;
+    }
+    @Test
+    public void test(){
+        assertEquals(new _111MinimumTreeDepth().minDepth(TreeUtil.convertArrayToTree(new Integer[]{1,2})), 2);
+        assertEquals(new _111MinimumTreeDepth().minDepth(TreeUtil.convertArrayToTree(new Integer[]{1})), 1);
+        assertEquals(new _111MinimumTreeDepth().minDepth(TreeUtil.convertArrayToTree(new Integer[]{1,null,2})), 2);
+        assertEquals(new _111MinimumTreeDepth().minDepth(TreeUtil.convertArrayToTree(new Integer[]{1,3,2,null,null,5,null})), 2);
+    }
+}
