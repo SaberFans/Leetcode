c864055b4f673e28464dea177300d26c184e7c11
twang123 <wtyang926@gmail.com>
2017-03-26 19:17:30 +0100

    [easy_tree] binary tree leaning on left.

--- src/_LeetcodeLoop/_108ConvertSortedArrayToBinarySearchTree.java
+++ src/_LeetcodeLoop/_108ConvertSortedArrayToBinarySearchTree.java
@@ -10,17 +10,14 @@ import org.junit.Test;
 public class _108ConvertSortedArrayToBinarySearchTree {
     public TreeNode sortedArrayToBST(int[] nums) {
         if(nums!=null){
-            return getNextNode(nums, 0, nums.length-1);
+            return getNextNode(nums, 0, nums.length);
         }
         return null;
     }
     private TreeNode getNextNode(int[] nums, int low, int high){
-        if(low>high)
+        if(low==nums.length || low>high)
             return null;
-        if(low==high){
-            return new TreeNode(nums[low]);
-        }
-        int mid = low+(high-low)/2;;
+        int mid = low+(high-low)/2;
         TreeNode root = new TreeNode(nums[mid]);
         root.left = getNextNode(nums, low, mid-1);
         root.right = getNextNode(nums, mid+1, high);
@@ -28,6 +25,6 @@ public class _108ConvertSortedArrayToBinarySearchTree {
     }
     @Test
     public void test(){
-        System.out.println(new _108ConvertSortedArrayToBinarySearchTree().sortedArrayToBST(new int[]{1,3}));
+        System.out.println(new _108ConvertSortedArrayToBinarySearchTree().sortedArrayToBST(new int[]{1,3,5,7,8,9,10,12}));
     }
 }
