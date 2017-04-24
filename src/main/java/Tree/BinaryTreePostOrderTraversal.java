02b601a281c8ddc35e9584aff81604c35690d0ae
twang123 <wtyang926@gmail.com>
2017-04-24 23:44:16 +0100

    [hard_tree] post order traversal.

--- src/main/java/Tree/BinaryTreePostOrderTraversal.java
+++ src/main/java/Tree/BinaryTreePostOrderTraversal.java
@@ -1,59 +1,76 @@
 package Tree;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class BinaryTreePostOrderTraversal {
 
     /**
-     * Keep a previous reference to keep the left/right child, to
-     * prevent duplicates nodes to add into Stack.
-     * *Needs more optimization on the code*
-     *
-     * @param root Top-most root
-     * @return post-Order traversal of binary tree.
+     * Keep a last visited tree node reference.
+     * @param root
+     * @return postorder traversal of binary tree.
      */
     public List<Integer> postOrderTraversal(TreeNode root) {
-        List<Integer> tree = new ArrayList<>();
-        Stack<TreeNode> nodes = new Stack<>();
-        if (root == null)
-            return tree;
+        List<Integer> ret = new ArrayList<>();
+        if(root!=null){
 
-        nodes.push(root);
-        TreeNode previous = root;
-        while (!nodes.isEmpty()) {
-            root = nodes.peek();
-            if (root.left == null && root.right == null) {
-                tree.add(root.val);
-                nodes.pop();
-                previous = root;
-            } else if (root.left == previous) {
-                if (root.right == null) {
-                    nodes.pop();
-                    tree.add(root.val);
-                    previous = root;
-                } else {
-                    nodes.push(root.right);
+            Deque<TreeNode> nodes = new ArrayDeque<>();
+            nodes.add(root);
+
+            TreeNode prev = null;
+
+            while(!nodes.isEmpty()){
+                TreeNode rt = nodes.peekLast();
+
+                // going down to fetch all the nodes.
+                if(prev==null || prev.left==rt|| prev.right==rt){
+                    if(rt.left!=null){
+                        nodes.add(rt.left);
+                    }
+                    else if(rt.right!=null){
+                        nodes.add(rt.right);
+                    }
+                    else{
+                        ret.add(nodes.pollLast().val);
+                    }
                 }
-            } else if (root.right == previous) {
-                nodes.pop();
-                tree.add(root.val);
-                previous = root;
-            } else {
-                if (root.left != null) {
-                    nodes.push(root.left);
-                } else if (root.right != null) {
-                    nodes.push(root.right);
+                // going up from left sub tree
+                else if(rt.left==prev){
+                    if(rt.right!=null){
+                        nodes.add(rt.right);
+                    }
+                    else{
+                        ret.add(nodes.pollLast().val);
+                    }
                 }
+                // going up from right sub tree
+                else if(rt.right==prev){
+                    ret.add(nodes.pollLast().val);
+                }
+
+                prev = rt;
             }
         }
-
-        return tree;
+        return ret;
     }
+    @Test
+    public void test_postorder_iterative1(){
+        TreeNode node = TreeUtil.convertArrayToTree(new Integer[]{1,2,3,4,5,6,7});
+        List<Integer> expected = Arrays.asList(4,5,2,6,7,3,1);
 
+        Assert.assertEquals(expected, new BinaryTreePostOrderTraversal().postOrderTraversal(node));
+        Assert.assertEquals(expected, new BinaryTreePostOrderTraversal().postOrderTraversalOptimized(node));
+    }
+    @Test
+    public void test_postorder_iterative2(){
+        TreeNode node = TreeUtil.convertArrayToTree(new Integer[]{1,2,3,null,5,6,7,8,9});
+        List<Integer> expected = Arrays.asList(8,9,5,2,6,7,3,1);
+
+        Assert.assertEquals(expected, new BinaryTreePostOrderTraversal().postOrderTraversal(node));
+        Assert.assertEquals(expected, new BinaryTreePostOrderTraversal().postOrderTraversalOptimized(node));
+    }
     /**
      * Iterative approach, in more neat manner.
      * Commented and Optimized.
@@ -71,13 +88,13 @@ public class BinaryTreePostOrderTraversal {
         while (!nodes.isEmpty()) {
             TreeNode curr = nodes.peek();
             if (curr != null) {
-                // Not Returning from left and right
+                // going down
                 if (curr.left != last && curr.right!=last) {
                     nodes.push(curr.left);
                     last = curr.left;
                 }
                 // Returning from left child, go to right child.
-                else if(curr.right!=last) {
+                else if(curr.left==last) {
                     nodes.push(curr.right);
                     last = curr.right;
                 }
@@ -87,8 +104,8 @@ public class BinaryTreePostOrderTraversal {
                     last = curr;
                 }
             } else {
-                nodes.pop();
-                if (nodes.peek().right == last || nodes.peek().right == null) {
+                nodes.pop();//nodes.peek().right == last ||
+                if (nodes.peek().right == null) {
                     postOrder.add(nodes.peek().val);
                     last = nodes.peek();
                     nodes.pop();
@@ -100,7 +117,6 @@ public class BinaryTreePostOrderTraversal {
         }
         return postOrder;
     }
-
 
     /**
      * Recursive way to visit the nodes.
@@ -114,20 +130,6 @@ public class BinaryTreePostOrderTraversal {
         postOrderTraversalRecur(root.right);
         postorderTree.add(root.val);
         return postorderTree;
-    }
-
-    @Test
-    public void test() {
-        TreeNode n1 = new TreeNode(1);
-        TreeNode n2 = new TreeNode(2);
-        TreeNode n3 = new TreeNode(3);
-        n1.left = null;
-        n1.right = n3;
-        n2.left = null;
-        n2.right = n3.left = n3.right = null;
-        List<Integer> tr = postOrderTraversalOptimized(n1);
-
-        System.out.println(tr);
     }
 
 }
